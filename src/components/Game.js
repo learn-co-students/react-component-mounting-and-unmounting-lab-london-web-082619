639da6550ccd8048635f3e25fdc247325572baab
github username <github email address>
2019-11-05 13:47:58 +0000

    Done.

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,8 +17,7 @@ class Game extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will set the current time
-  
-  setCurrentTime = () => {
+  componentDidMount() {
     this.setState({ time: new Date(Date.now())});
   }
 
